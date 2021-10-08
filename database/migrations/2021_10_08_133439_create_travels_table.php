c9e8df69bbda589c14230413a7cfa38f1bb2b07f
daniele-pipino <Danielemacpipino@icloud.com>
2021-10-08 15:54:00 +0200

    added a migrations to database with some touple

---
+++ database/migrations/2021_10_08_133439_create_travels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 100);
+            $table->float('price', 8, 2)->unsigned();
+            $table->string('title', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
